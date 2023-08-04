2e200b57e2ef79d54777860b2292cfd1b70ab8e5
Casv2302 <charlesandresv@hotmail.com>
2023-08-04 11:53:58 -0300

    04/08/2023 - Implementei recursos de formatação de um blog com o Chat GPT

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,14 @@ from flask import render_template
 def index():
     return render_template('index.html', titulo = 'Pagina Inicial')
 
-@app.route('/contatos')
-def contatos():
-    return render_template('contatos.html', titulo = 'Contatos')
+@app.route('/contato')
+def contato():
+    return render_template('contato.html', titulo = 'Contato')
+
+@app.route('/artigos')
+def artigo():
+    return render_template('artigos.html', titulo = 'artigos')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html', titulo = 'Sobre')
+    return render_template('sobre.html', titulo = 'sobre')        
